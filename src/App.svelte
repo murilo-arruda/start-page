cb0966f27fd5f48a203a55d79d9fb0a3cd3d7b9d
Murilo Arruda <muriloarrudap@gmail.com>
2020-08-22 12:43:59 -0300

    initial config

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import LinkBox from "./components/LinkBox.svelte";
+</script>
+
+<style>
+  main {
+    margin: 0 auto;
+    max-width: 1080px;
+    padding-top: 150px;
+  }
+
+  .wrapper {
+    padding: 1em;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+  }
+</style>
+
+<main>
+
+  <input class="nes-input" type="text" placeholder="searchbar" />
+  <div class="wrapper">
+
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+    <LinkBox title="some title" />
+
+  </div>
+</main>
---
+++ src/components/LinkBox.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let title;
+
+  let links = [
+    { name: "reddit", url: "https://www.reddit.com" },
+    { name: "youtube", url: "https://www.youtube.com" },
+    { name: "youtube", url: "https://www.youtube.com" },
+    { name: "youtube", url: "https://www.youtube.com" },
+  ];
+</script>
+
+<style>
+  section {
+    margin: 5px;
+  }
+</style>
+
+<section class="nes-container with-title">
+
+  <p class="title">{title || ''}</p>
+
+  <ul class="nes-list is-circle">
+    {#each links as link}
+      <li>
+
+        <a href={link.url} class="nes-text is-primary">{link.name}</a>
+      </li>
+    {/each}
+  </ul>
+</section>
